c8e3017e0ef51162e5902852e8ff89b5922ad3cb
ket36 <dinhvanket36@gmail.com>
2024-10-12 17:12:46 +0700

    5:12 12/10/2024
    login và register

--- src/app/dangky/page.tsx
+++ src/app/dangky/page.tsx
@@ -1,86 +1,3 @@
-// import React, { useState } from 'react';
-// import './signup.css';
-// import RadioGroup from '@/components/radio';
-
-// const PageSignUp = () => {
-//   return (
-//     <div className="register-wrapper">
-//       <div className="register-container">
-//         <img src={'./images/trend.png'} alt="VJobs logo"
-//           style={{
-//             marginLeft: '0px', width: 'auto', height: '40px', fontSize: '1.5rem',
-//             marginTop: '-20px', flexShrink: '0'
-//           }} />
-//         <h2>Đăng Ký</h2>
-//         <br/>
-//         <form className="register-form">
-//           <div className='row'>
-//             <div className='col-6'>
-//               <div className="input-register">
-//                 <label>Họ tên</label>
-//                 <input type="text" placeholder="Họ và tên" />
-//               </div>
-//               <div className="input-register">
-//                 <label>Email</label>
-//                 <input type="email" placeholder="Địa chỉ email" />
-//               </div>
-//               <div className="input-register">
-//                 <label>Mật khẩu</label>
-//                 <input type="password" placeholder="********" />
-//               </div>
-//               <div className="input-register">
-//                 <label>SĐT</label>
-//                 <input type="text" placeholder="Số điện thoại" />
-//               </div>
-
-//             </div>
-//             <div className='col-7'>
-//               <div className="input-register">
-//                 <label>Địa chỉ</label>
-//                 <input type="text" placeholder="Địc chỉ" />
-//               </div>
-//               <div className="input-register">
-//                 <label>Năm sinh</label>
-//                 <input type="date" placeholder="năm sinh" />
-//               </div>
-//               <div className='rolessss'>
-//                 <label>Giới tính</label>
-//                 <br />
-//                 <RadioGroup/>
-//               </div>
-//             </div>
-//           </div>
-
-//           <br />
-//           <div className="checkbox-register">
-//             <input type="checkbox" className="remembers" id="remember" />
-//             <label htmlFor="remember">Tôi muốn đăng ký nhận thông báo qua email.</label>
-//           </div>
-//           <button type="submit" className="btn-register">Đăng ký</button>
-//         </form>
-//         <div className="signup-register">
-//           Đã có tài khoản? <a href="/login">Đăng nhập</a>
-//         </div>
-//         <div className="separator">hoặc</div>
-//         <div className="social-register">
-//           <button className="btn-google">
-//             <img src={'./images/icons8-google-48.png'} alt="Google Icon"
-//               style={{ width: '25px', marginRight: '8px' }} />
-//             Đăng ký với Google
-//           </button>
-//           <br />
-//           <button style={{ marginTop: '-3px' }} className="btn-facebook">
-//             <img src={'./images/icons8-facebook-48.png'} alt="Facebook Icon"
-//               style={{ width: '25px', marginRight: '8px' }} />
-//             Đăng ký với Facebook
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PageSignUp;
 "use client";
 import React, { useState, useEffect } from "react";
 import "./signup.css";
@@ -166,7 +83,7 @@ const PageSignUp = () => {
     // Kiểm tra form trước khi tiếp tục
     if (!validateForm()) return;
     try {
-      const response = await fetch("http://localhost:8080/api/register", {
+      const response = await fetch("http://localhost:8080/api/dangKy", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
